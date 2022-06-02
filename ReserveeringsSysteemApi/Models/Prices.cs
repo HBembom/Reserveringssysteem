03d735e38af2a25f083d17882268c940b818fdf6
Jordy Neef <43172462+JordyNeef@users.noreply.github.com>
2022-06-02 21:27:05 +0200

    Price Routes added + added files to gitignore

---
+++ ReserveeringsSysteemApi/Models/Prices.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+using MySqlConnector;
+using ReserveeringsSysteemApi.Properties;
+
+namespace ReserveeringsSysteemApi.Models
+{
+    public class Prices
+    {
+        public int PriceId { get; set; }
+        public double Amount { get; set; }
+        public string Name { get; set; }
+        public bool IsTax { get; set; }
+        internal DbConnector Connector { get; set; }
+
+        public Prices(DbConnector connector)
+        {
+            Connector = connector;
+        }
+
+        public Prices() { } //Necessary for deserialization
+
+        public async Task InsertPrice()
+        {
+            await using var command = Connector.Conn.CreateCommand();
+            command.CommandText = "INSERT INTO `prices` (Amount, Name, IsTax) VALUES (@Amount, @Name, @IsTax)";
+            AddPricesParameters(command);
+            await command.ExecuteNonQueryAsync();
+            PriceId = (int)command.LastInsertedId;
+        }
+
+        public async Task<Prices> SelectPriceById(int id)
+        {
+            await using var command = Connector.Conn.CreateCommand();
+            command.CommandText = @"SELECT * FROM `prices` WHERE `PriceId` = @PriceId";
+            command.Parameters.Add(new MySqlParameter
+            {
+                ParameterName = "@PriceId",
+                DbType = DbType.Int32,
+                Value = id,
+            });
+            var res = await ReadAllPrices(await command.ExecuteReaderAsync());
+
+            return res.Count > 0 ? res[0] : null;
+        }
+
+        public async Task<List<Prices>> SelectAllPrices()
+        {
+            await using var command = Connector.Conn.CreateCommand();
+            command.CommandText = @"SELECT * FROM prices";
+            var res = await ReadAllPrices(await command.ExecuteReaderAsync());
+
+            return res.Count > 0 ? res : null;
+        }
+
+        public async Task UpdatePriceById()
+        {
+            await using var command = Connector.Conn.CreateCommand();
+            command.CommandText = @"UPDATE `prices` SET AccommodationType = @Amount, @Name, @IsTax WHERE PriceId = @PriceId";
+            AddPricesParameters(command);
+            AddPricesId(command);
+            await command.ExecuteNonQueryAsync();
+        }
+
+        public async Task DeletePriceById()
+        {
+            await using var command = Connector.Conn.CreateCommand();
+            command.CommandText = @"DELETE FROM `prices` WHERE PriceId = @PriceId";
+            AddPricesId(command);
+            await command.ExecuteNonQueryAsync();
+        }
+
+        private void AddPricesParameters(MySqlCommand command)
+        {
+            command.Parameters.Add(new MySqlParameter
+            {
+                ParameterName = @"Amount",
+                DbType = DbType.Int32,
+                Value = Amount
+            });
+            command.Parameters.Add(new MySqlParameter
+            {
+                ParameterName = @"Name",
+                DbType = DbType.String,
+                Value = Name
+            });
+            command.Parameters.Add(new MySqlParameter
+            {
+                ParameterName = @"IsTax",
+                DbType = DbType.Boolean,
+                Value = IsTax
+            });
+        }
+
+        private void AddPricesId(MySqlCommand command)
+        {
+            command.Parameters.Add(new MySqlParameter
+            {
+                ParameterName = @"PriceId",
+                DbType = DbType.Int32,
+                Value = PriceId
+            });
+        }
+
+        private async Task<List<Prices>> ReadAllPrices(DbDataReader reader)
+        {
+            var prices = new List<Prices>();
+            await using (reader)
+            {
+                while (await reader.ReadAsync())
+                {
+                    var price = new Prices(Connector)
+                    {
+                        PriceId = reader.GetInt32(0),
+                        Amount = reader.GetInt32(1),
+                        Name = reader.GetString(2),
+                        IsTax = reader.GetBoolean(3)
+                    };
+                    prices.Add(price);
+                }
+            }
+
+            return prices;
+        }
+    }
+}
