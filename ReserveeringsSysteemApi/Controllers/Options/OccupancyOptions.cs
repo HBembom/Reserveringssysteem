f7f0aed7908c1776ece25906c19bd9efda4de2e7
HBembom <56027034+HBembom@users.noreply.github.com>
2022-06-13 17:27:42 +0200

    Comit deleted changes

--- ReserveeringsSysteemApi/Controllers/Options/OccupancyOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ReserveeringsSysteemApi.Controllers.Options
-{
-    public class OccupancyOptions
-    {
-        public string StartDate { get; set; }
-        public string EndDate { get; set; }
-        public bool NewDateFirst { get; set; }
-    }
-}
