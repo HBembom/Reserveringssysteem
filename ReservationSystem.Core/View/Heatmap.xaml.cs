082a783c43bddf763691cb80bf44328dd093f1b9
Kariemdv <56026990+Kariemdv@users.noreply.github.com>
2022-06-27 16:06:29 +0200

    Started on heatmap
    Made a start on the heatmap, binding colors to gridItems, using test colors for now.

---
+++ ReservationSystem.Core/View/Heatmap.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI;
+using System.Diagnostics;
+
+namespace ReservationSystem.Core.View
+{
+    public sealed partial class Heatmap : Page
+    {
+        private SolidColorBrush _1_7HeatmapColor { get; set; }
+        private SolidColorBrush _8_11HeatmapColor { get; set; }
+        private SolidColorBrush _12_15HeatmapColor { get; set; }
+        private SolidColorBrush _16_22HeatmapColor { get; set; }
+        private SolidColorBrush _23_29HeatmapColor { get; set; }
+
+        public Heatmap()
+        {
+            this.InitializeComponent();
+            UpdateHeatmapColors();
+        }
+
+        public void UpdateHeatmapColors()
+        {
+            _1_7HeatmapColor = new SolidColorBrush(Color.FromArgb(200, 255, 45, 45));
+            _8_11HeatmapColor = new SolidColorBrush(Color.FromArgb(200, 45, 255, 45));
+            _12_15HeatmapColor = new SolidColorBrush(Color.FromArgb(200, 45, 45, 255));
+            _16_22HeatmapColor = new SolidColorBrush(Color.FromArgb(200, 255, 255, 255));
+            _23_29HeatmapColor = new SolidColorBrush(Color.FromArgb(200, 45, 45, 45));
+        }
+    }
+}
