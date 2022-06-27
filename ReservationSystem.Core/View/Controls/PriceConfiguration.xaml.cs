4ec9c01cd9618db9db8e96fd6e6bdb85e18cf9d6
HBembom <56027034+HBembom@users.noreply.github.com>
2022-06-27 11:49:37 +0200

    Comit Changes

--- ReservationSystem.Core/View/Controls/PriceConfiguration.xaml.cs
+++ ReservationSystem.Core/View/Controls/PriceConfiguration.xaml.cs
@@ -20,9 +20,9 @@ namespace ReservationSystem.Core.View.Controls
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class PriceConfigurationControl : Page
+    public sealed partial class PriceConfiguration : UserControl
     {
-        public PriceConfigurationControl()
+        public PriceConfiguration()
         {
             this.InitializeComponent();
         }
